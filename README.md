e216a4f8512265159565eabaa8450a17b02de45b
Roy Wright <roy.w.wright@gmail.com>
2018-12-06 14:28:19 -0600

    Some tweaks and cleanup

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ I noticed that sometimes a bot in [Eternal Lands](http://eternal-lands.com/) is 
 `get_EMUs.py` processes the wiki scrapings down to a list of items and weights and stores them as `weights.pkl`. This file is provided here, so there should be no need to actually run `scrape_elwiki.py` or `get_EMUs.py`.
 
 `opportunities.ipynb` gets all current bot and NPC prices from [here](http://greypal.el-fd.org/cgi-bin/querybot) and lists the best profit-making opportunities.
+
+`summoning.ipynb` is a later, separate, but closely related effort to make informed decisions about which game creatures are the most efficient means for gaining experience with the in-game skill of "summoning."
--- get_EMUs.py
+++ get_EMUs.py
@@ -8,28 +8,25 @@ with open('pages.pkl', 'rb') as f:
     pages = pickle.load(f)
 
 # Get every item's wiki page title, weight, and stackability
-#   o Wiki page title is essentially the last bit of the URL
-#   o Weight is the smallest number mentioned with "EMU" after it
-#   o Stackability is usually either "yes" or "no"
 weights = [
     (
-        p[0].replace(
-            'http://www.el-wiki.net/',''
-        ).replace('_',' ').replace('%26','&'), 
-        min([
-            int(n) 
-            for n in re.findall('(\d+)\s*emu', str(p[1]).lower())
-        ], default = 1), 
+        # Wiki page title is essentially the last bit of the URL
+        p[0].replace('http://www.el-wiki.net/','')
+            .replace('_',' ').replace('%26','&'), 
+      
+        # Weight is the smallest number mentioned with "EMU" after it
+        min([int(n) 
+            for n in re.findall('(\d+)\s*emu', str(p[1]).lower())], 
+            default = 1), 
+
+        # Stackability is usually either "yes" or "no"
         ','.join(re.findall('stackable:\s*([a-z]+)', str(p[1]).lower()))
     ) 
     for p in pages
 ]
 
 # Structure the item information as a dataframe
-weights = pd.DataFrame(
-    weights, 
-    columns = ['WikiPage', 'Weight', 'Stackable']
-)
+weights = pd.DataFrame(weights, columns = ['WikiPage', 'Weight', 'Stackable'])
 
 # No real item should have weight 0, so change it to 1
 weights.loc[weights.Weight == 0, 'Weight'] = 1
